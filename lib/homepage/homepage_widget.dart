4b939ef6feaefcb9fdad6c2a943ac791f67186d1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-15 10:50:25 +0000

    Updating to latest FlutterFlow output.

--- lib/homepage/homepage_widget.dart
+++ lib/homepage/homepage_widget.dart
@@ -38,20 +38,23 @@ class _HomepageWidgetState extends State<HomepageWidget> {
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         key: scaffoldKey,
-        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-        body: Container(
-          width: MediaQuery.sizeOf(context).width * 1.0,
-          height: MediaQuery.sizeOf(context).height * 1.0,
-          decoration: BoxDecoration(
-            color: FlutterFlowTheme.of(context).primary,
-          ),
-          child: FlutterFlowWebView(
-            content: 'https://hostinglab.cloud',
-            bypass: false,
+        backgroundColor: FlutterFlowTheme.of(context).primary,
+        body: SafeArea(
+          top: true,
+          child: Container(
             width: MediaQuery.sizeOf(context).width * 1.0,
             height: MediaQuery.sizeOf(context).height * 1.0,
-            verticalScroll: true,
-            horizontalScroll: true,
+            decoration: BoxDecoration(
+              color: FlutterFlowTheme.of(context).primary,
+            ),
+            child: FlutterFlowWebView(
+              content: 'https://hostinglab.cloud',
+              bypass: false,
+              width: MediaQuery.sizeOf(context).width * 1.0,
+              height: MediaQuery.sizeOf(context).height * 1.0,
+              verticalScroll: true,
+              horizontalScroll: true,
+            ),
           ),
         ),
       ),
